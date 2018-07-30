9507438c2914cfd7deb9f62320a9314e24f5181a
Adriana Mora <AMHymyilee@users.noreply.github.com>
2018-07-30 18:36:29 -0500

    funcion editar y cambios en el diseño del muro

--- src/js/main.js
+++ src/js/main.js
@@ -9,7 +9,7 @@ const logout = document.getElementById('logout').addEventListener('click', event
 
 const publicar = () => {
   firebase.auth().onAuthStateChanged(user => {
-    if (user) {  
+    if (user) {
       document.getElementById('publish').addEventListener('click', event => { // Evento que detona el botón de 'publicar'
         event.preventDefault();
         let postDate = firebase.firestore.FieldValue.serverTimestamp();
@@ -36,9 +36,9 @@ const publicar = () => {
 };
 
 function deletePost(id) {
-  db.collection('post').doc(id).delete().then(function() {
+  db.collection('post').doc(id).delete().then(function () {
     console.log('Document successfully deleted!');
-  }).catch(function(error) {
+  }).catch(function (error) {
     console.error('Error removing document: ', error);
   });
 };
@@ -47,21 +47,21 @@ function deletePost(id) {
 const drawPost = () => {
   const postContainer = document.getElementById('publications'); // Este id es el contenedor que pinta las publicaciones.
   db.collection('post').onSnapshot((querySnapshot) => { // onSnapshot es un agente de escucha, que va a estar 'escuchando' cada que se haga un cambio en la base de datos.
-    querySnapshot.forEach((doc) => { 
+    querySnapshot.forEach((doc) => {
       postContainer.innerHTML += `<div class="card card-post card-round" my-5 px-2>
                                    <div class="card-header1" id= "${doc.id}">
                                      <h5> <i class="fas fa-user-circle"></i> ${doc.data().userID}</h5>
                                      <p>${doc.data().date}</p>
                                     </div>
                                     <div class="card-body">
-                                      <h6 class="card-text">${doc.data().content}</h6>
+                                      <h6 class="card-text" id="user${doc.id}">${doc.data().content}</h6>
                                     </div>
                                     <div class="card-footer1 mr-auto">
                                     <div class="btn-group" role="group" aria-label="Basic example">
                                        <button class="btn btn-info1  float-right">
                                          <i class="fas fa-heart"></i> 0
                                        </button>
-                                       <button class="btn btn-info1 float-right">
+                                       <button class="btn btn-info1 float-right" onclick ="editPost('${doc.id}')">
                                         <i class="fas fa-edit"></i> Editar
                                        </button>
                                        <button class="btn btn-info1 float-right" onclick ="deletePost('${doc.id}')">
@@ -69,14 +69,31 @@ const drawPost = () => {
                                        </button>
                                     </div>
                                    </div>
-                                    <div class="card-footer1 mr-auto">
-                                      <input id="comment" type="text"> 
-                                      <button class="btn btn-info1 float-right"><i class="fas fa-comment-alt"></i></button>
-                                      </div>
                                 </div>
                                 <br/>`;
     });
   });
 };
+
+const editPost = (id) => { // Id es el parámetro del post a editar
+  db.collection('post').doc(id).get() // entra a post y entra en el comentario con el ID especificado y lo llama
+    .then(res => { // Obtiene la respuesta
+      const currentContent = res.data().content; // Declaramos variable que contiene al post
+      document.getElementById(`user${res.id}`).innerHTML = `<textarea cols="30" id="text${res.id}">${currentContent} </textarea> <button class="btn btn-info1 float-right" onclick ="saveEditedPost('${res.id}')"><i class="fas fa-save"></i></button> `;
+    }); // Imprime un textarea con el contenido del post para habilitar la edición y llama a la función "saveEditedPost"
+};
+
+const saveEditedPost = (id) => {
+  const newContent = document.getElementById(`text${id}`).value; // Guarda los nuevos datos del post
+  console.log(newContent);
+  db.collection('post').doc(id).update({ // Actualizamos el post con el id especificado 
+    content: newContent // Le dice a firebase que el contenido del post es equivalente al nuevo contenido
+  }).then(res => {
+    alert('Publicacion Editada'); // Muestra una alerta de que la edicion fue exitosa
+    drawPost(); // llama a drawPost para que pinte el post actualizado
+  });
+};
+
+
 publicar();
 drawPost();
